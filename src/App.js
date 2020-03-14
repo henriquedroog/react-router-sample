e29b098b0db98cac6f1573e4ee8c4eefed8af275
Henrique Droog <corneliohenrique.droog@prodyna.com>
2020-03-14 19:52:35 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Fragment} from 'react';
 import './App.css';
+import{ BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const name = 'John Doe';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main>
+        <nav>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to={`/about/${name}`}>About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about/:name"  component={About} />
+          <Route path="/contact"  component={Contact} />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
-export default App;
+// Home Page
+const Home = () => (
+  <Fragment>
+    <h1>Home</h1>
+    <FakeText />
+  </Fragment>
+);
+// About Page
+const About = ({match:{params:{name}}}) => (
+  // props.match.params.name
+  <Fragment>
+    <h1>About {name}</h1>
+    <FakeText />
+  </Fragment>
+);
+// Contact Page
+const Contact = () => (
+  <Fragment>
+    <h1>Contact</h1>
+    <FakeText />
+  </Fragment>
+);
+
+const FakeText = () => (
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+)
+// export default App;
